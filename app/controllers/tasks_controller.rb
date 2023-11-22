7af2c967eca8358b5bf397d32a1d7ab7bb8b0ce2
はらなおみ <haraweb7@gmail.com>
2023-11-22 16:56:14 +0900

    削除機能完成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -39,6 +39,13 @@ class TasksController < ApplicationController
     end
   end
 
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, notice:"削除しました！"
+  end
+
+
   private
 
   def task_params
