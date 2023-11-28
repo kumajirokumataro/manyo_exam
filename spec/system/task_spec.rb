5a035b0b2d41f82e3f5e9dec6ca31b32a07be45e
はらなおみ <haraweb7@gmail.com>
2023-11-28 18:09:11 +0900

    step2提出

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -6,12 +6,23 @@ RSpec.describe 'タスク管理機能', type: :system do
         visit new_task_path
         fill_in 'task_name', with: 'パパ'
         fill_in 'task_content', with: '日曜日歯医者'
-        click_on 'Create Task'
+        click_on '登録する'
         expect(page).to have_content '日曜日歯医者'
 
       end
     end
+    context 'タスクが作成日時の降順に並んでいる場合' do
+      it '新しいタスクが一番上に表示される' do
+        FactoryBot.create(:task, content: '自転車修理')
+        FactoryBot.create(:task, content: '買い物')
+        FactoryBot.create(:task, content: '洗濯')
+        visit tasks_path
+        task_list = all('.task_list')[0] 
+        expect(task_list).to have_content '洗濯'        
+      end
+    end
   end
+
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
