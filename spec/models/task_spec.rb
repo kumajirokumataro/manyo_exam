d0d7eb64584997b12a1f2d26ac956476daf62027
はらなおみ <haraweb7@gmail.com>
2023-12-14 14:08:37 +0900

    モデルスペック修正

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,21 +2,22 @@ require 'rails_helper'
 
 RSpec.describe 'タスクモデル機能', type: :model do
   describe 'バリデーションのテスト' do
+    let!(:user) { FactoryBot.create(:user) }
     context 'タスクのタイトルが空の場合' do
       it 'バリデーションにひっかる' do
-        task = Task.new(name: '', content: '失敗テスト')
+        task = Task.new(name: '', content: '失敗テスト', user: user)
         expect(task).not_to be_valid
       end
     end
     context 'タスクの詳細が空の場合' do
       it 'バリデーションにひっかかる' do
-        task = Task.new(name: '失敗テスト', content: '')
+        task = Task.new(name: '失敗テスト', content: '', user: user)
         expect(task).not_to be_valid
       end
     end
     context 'タスクのタイトルと詳細に内容が記載されている場合' do
       it 'バリデーションが通る' do
-        task = Task.new(name: '成功テスト', content: '成功テスト')
+        task = Task.new(name: '成功テスト', content: '成功テスト', user: user)
         expect(task).to be_valid
       end
     end
