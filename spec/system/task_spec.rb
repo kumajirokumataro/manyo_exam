d500f70cc8cd47691ca087a061dccff6e7e82860
はらなおみ <haraweb7@gmail.com>
2023-12-14 14:40:23 +0900

    提出最終

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,34 +1,47 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
   describe '新規作成機能' do
+    let!(:user) { FactoryBot.create(:user) }
+    before do
+      visit new_session_path
+      sleep(1)
+      fill_in 'session_email', with: user.email
+      fill_in 'session_password', with: user.password
+      click_on 'Log in'
+      sleep(1)
+    end
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される(新規登録が正常に完了する)' do
         visit new_task_path
+        sleep(1)
         fill_in 'task_name', with: 'パパ'
         fill_in 'task_content', with: '日曜日歯医者'
         fill_in 'task_timelimit', with: '002023-12-17'
         find("#task_status").find("option[value='完了']").select_option
         click_on '登録する'
+        sleep(1)
         expect(page).to have_content '完了'
 
       end
     end
     context 'タスクが作成日時の降順に並んでいる場合' do
       it '新しいタスクが一番上に表示される' do
-        FactoryBot.create(:task, content: '自転車修理')
-        FactoryBot.create(:task, content: '買い物')
-        FactoryBot.create(:task, content: '洗濯')
+        FactoryBot.create(:task, content: '自転車修理', user: user)
+        FactoryBot.create(:task, content: '買い物', user: user)
+        FactoryBot.create(:task, content: '洗濯', user: user)
         visit tasks_path
+        sleep(1)
         task_list = all('.task_list')[0] 
         expect(task_list).to have_content '洗濯'        
       end
     end
     context 'タスクが終了期限の降順に並び替えられた場合' do
       it '新しいタスクが一番上に表示される' do
-        FactoryBot.create(:task, timelimit: '002024-01-10')
-        FactoryBot.create(:task )
-        FactoryBot.create(:task, timelimit: '002023-12-15')
+        FactoryBot.create(:task, timelimit: '002024-01-10', user: user)
+        FactoryBot.create(:task, user: user)
+        FactoryBot.create(:task, timelimit: '002023-12-15', user: user)
         visit tasks_path
+        sleep(1)
         click_on '終了期限でソートする'
         sleep(1)
         task_list = all('.task_list')[0]
@@ -37,10 +50,11 @@ RSpec.describe 'タスク管理機能', type: :system do
     end  
     context 'タスクが優先順位の高い順に並び替えられた場合' do
       it '優先順位「高」のタスクが一番上に表示される' do
-        FactoryBot.create(:task, rank: '低')
-        FactoryBot.create(:task )
-        FactoryBot.create(:task, rank: '高')
+        FactoryBot.create(:task, rank: '低', user: user)
+        FactoryBot.create(:task, user: user )
+        FactoryBot.create(:task, rank: '高', user: user)
         visit tasks_path
+        sleep(1)
         click_on '優先順位でソートする'
         sleep(1)
         task_list = all('.task_list')[0]
@@ -50,37 +64,63 @@ RSpec.describe 'タスク管理機能', type: :system do
   end
 
   describe '一覧表示機能' do
+    let!(:user) { FactoryBot.create(:user) }
+    before do
+      visit new_session_path
+      sleep(1)
+      fill_in 'session_email', with: user.email
+      fill_in 'session_password', with: user.password
+      click_on 'Log in'
+      sleep(1)
+    end
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
-        FactoryBot.create(:task, name: 'task')
+        FactoryBot.create(:task, name: 'task', user: user)
         visit tasks_path
         expect(page).to have_content 'task'
       end
     end
   end
   describe '詳細表示機能' do
+    let!(:user) { FactoryBot.create(:user) }
+    before do
+      visit new_session_path
+      sleep(1)
+      fill_in 'session_email', with: user.email
+      fill_in 'session_password', with: user.password
+      click_on 'Log in'
+      sleep(1)
+    end
      context '任意のタスク詳細画面に遷移した場合' do
        it '該当タスクの内容が表示される' do
-        FactoryBot.create(:task, name: '家事')
+        FactoryBot.create(:task, name: '家事', user: user)
         visit task_path(Task.last)
         expect(page).to have_content '家事'
        end
      end
   end
   describe '検索機能' do
+    let!(:user) { FactoryBot.create(:user) }
     before do
-      FactoryBot.create(:task, name: "おじいちゃん")
-      FactoryBot.create(:task, content: "掃除機" ,status: "未着手")
-      FactoryBot.create(:task, content: "お歳暮準備" ,status: "完了")
+      FactoryBot.create(:task, name: "おじいちゃん", user: user)
+      FactoryBot.create(:task, content: "掃除機" ,status: "未着手", user: user)
+      FactoryBot.create(:task, content: "お歳暮準備" ,status: "完了", user: user)
+      visit new_session_path
+      sleep(1)
+      fill_in 'session_email', with: user.email
+      fill_in 'session_password', with: user.password
+      click_on 'Log in'
+      sleep(1)
     end
-
     context 'タイトルであいまい検索をした場合' do
       it "検索キーワードを含むタスクで絞り込まれる" do
         visit tasks_path
+        sleep(1)
         # タスクの検索欄に検索ワードを入力する (例: task)
         fill_in 'task_name', with: 'おじいちゃん'
         # 検索ボタンを押す
         click_on '検索'
+        sleep(1)
         expect(page).to have_content 'おじいちゃん'
         expect(page).not_to have_content 'パパ'
       end
@@ -90,6 +130,7 @@ RSpec.describe 'タスク管理機能', type: :system do
         visit tasks_path
         find("#task_status").find("option[value='未着手']").select_option
         click_on '検索'
+        sleep(1)
         expect(page).to have_content '未着手'
         expect(page).not_to have_content 'お歳暮準備'
       end
@@ -97,6 +138,7 @@ RSpec.describe 'タスク管理機能', type: :system do
     context 'タイトルのあいまい検索とステータス検索をした場合' do
       it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる" do
         visit tasks_path
+        sleep(1)
         fill_in 'task_name', with: 'パパ'
         find("#task_status").find("option[value='未着手']").select_option
         expect(page).to have_content '未着手'
--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -4,11 +4,13 @@ RSpec.describe 'ユーザー機能', type: :system do
     context 'ユーザー新規登録した場合' do
       it '新規登録が正常に完了する' do
         visit new_user_path
+        sleep(1)
         fill_in 'user_name', with: 'kumiko'
         fill_in 'user_email', with: 'kumiko@gmail'
         fill_in 'user_password', with: 'ninniku'
         fill_in 'user_password_confirmation', with: 'ninniku'
         click_on 'Create my account'
+        sleep(1)
         expect(page).to have_content 'kumikoのページ'
         expect(page).to have_content 'kumiko@gmail'
       end
@@ -16,6 +18,7 @@ RSpec.describe 'ユーザー機能', type: :system do
     context 'ログインせずにタスク一覧にとぼうとした場合' do
       it 'ログイン画面に遷移する' do
         visit tasks_path
+        sleep(1)
         expect(page).to have_content 'Log in'        
       end
     end
@@ -27,9 +30,11 @@ RSpec.describe 'ユーザー機能', type: :system do
     let!(:other_task) { FactoryBot.create(:second_task, user: other_user) }
     before do
       visit new_session_path
+      sleep(1)
       fill_in 'session_email', with: user.email
       fill_in 'session_password', with: user.password
       click_on 'Log in'
+      sleep(1)
     end
     context 'ログインした場合' do
       it "正常にログインする" do
@@ -40,7 +45,9 @@ RSpec.describe 'ユーザー機能', type: :system do
     context 'ログインした場合' do
       it "自分の詳細ページに飛べること" do
         visit tasks_path
+        sleep(1)
         click_on 'プロフィール'
+        sleep(1)
         expect(page).to have_content "#{user.name}のページ"
         expect(page).to have_content "#{user.email}"         
       end
@@ -48,6 +55,7 @@ RSpec.describe 'ユーザー機能', type: :system do
     context '他人の詳細画面に飛んだ場合' do
       it "自分のタスク一覧画面に遷移すること" do
         visit user_path(other_user.id)
+        sleep(1)
         expect(page).to have_content 'パパ'
         expect(page).to have_content '日曜日歯医者'
       end
@@ -55,6 +63,7 @@ RSpec.describe 'ユーザー機能', type: :system do
     context 'ログアウトした場合' do
       it "正常にログアウトが完了する" do
         click_on 'ログアウト'
+        sleep(1)
         expect(page).to have_content 'Log in'
       end
     end
@@ -67,13 +76,16 @@ RSpec.describe 'ユーザー機能', type: :system do
       let!(:other_task) { FactoryBot.create(:second_task, user: other_user) }
     before do
       visit new_session_path
+      sleep(1)
       fill_in 'session_email', with: other_user.email
       fill_in 'session_password', with: other_user.password
       click_on 'Log in'
+      sleep(1)
     end
       context '管理ユーザーが管理画面にアクセスした場合' do
       it "管理画面にアクセスできること" do
         visit admin_users_path
+        sleep(1)
         expect(page).to have_content user.name
         expect(page).to have_content other_user.name
       end
@@ -81,10 +93,13 @@ RSpec.describe 'ユーザー機能', type: :system do
       context '一般ユーザーが管理画面にアクセスした場合' do
       it "管理画面にアクセスできないこと" do
         click_on 'ログアウト'
+        sleep(1)
         fill_in 'session_email', with: user.email
         fill_in 'session_password', with: user.password
         click_on 'Log in'
+        sleep(1)
         visit admin_users_path
+        sleep(1)
         expect(page).to have_content '終了期限でソートする'
         expect(page).to have_content '優先順位でソートする'
       end
@@ -92,11 +107,13 @@ RSpec.describe 'ユーザー機能', type: :system do
     context '管理ユーザーがユーザーの新規登録をした場合' do
       it "正常に新規登録ができること" do
         visit new_user_path
+        sleep(1)
         fill_in 'user_name', with: 'kumiko'
         fill_in 'user_email', with: 'kumiko@gmail.com'
         fill_in 'user_password', with: 'ninniku'
         fill_in 'user_password_confirmation', with: 'ninniku'
         click_on 'Create my account'
+        sleep(1)
         expect(page).to have_content 'kumikoのページ'
         expect(page).to have_content 'kumiko@gmail.com'
       end
@@ -105,6 +122,7 @@ RSpec.describe 'ユーザー機能', type: :system do
     context '管理ユーザーがユーザー詳細画面にアクセスした場合' do
       it "詳細画面にアクセスできること" do
         visit user_path(user.id)
+        sleep(1)
         expect(page).to have_content "#{user.name}のページ"
         expect(page).to have_content "#{user.email}" 
       end
@@ -112,18 +130,21 @@ RSpec.describe 'ユーザー機能', type: :system do
     context '管理ユーザーが編集画面からユーザーを編集した場合' do
       it "編集できること" do
         visit edit_admin_user_path(user.id)
+        sleep(1)
         fill_in 'user_name', with: 'yuko'
         fill_in 'user_email', with: 'yuko@gmail'
         find("#user_admin").find("option[value='true']").select_option
         fill_in 'user_password', with: 'coffee'
         fill_in 'user_password_confirmation', with: 'coffee'
         click_on 'Create account'
+        sleep(1)
         expect(page).to have_content 'yuko@gmail'
       end
     end
     context '管理ユーザーがユーザーを削除した場合' do
       it "正常に削除できること" do
         visit admin_users_path
+        sleep(1)
         all('.btn.btn-warning')[0].click
         sleep(2)
         page.driver.browser.switch_to.alert.accept
@@ -134,4 +155,3 @@ RSpec.describe 'ユーザー機能', type: :system do
     end
   end
 end
-  #FactoryBot.create(:task, content: "お歳暮準備" ,status: "完了")
